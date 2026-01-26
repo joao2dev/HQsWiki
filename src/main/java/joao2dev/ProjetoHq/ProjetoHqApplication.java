634d5c9e7b131c3ef3fbd831f0e6a0c71613380c
Joao2dev <beneficil57@gmail.com>
2026-01-25 21:58:14 -0300

    iniciando projeto

---
+++ src/main/java/joao2dev/ProjetoHq/ProjetoHqApplication.java
@@ -0,0 +1,13 @@
+package joao2dev.ProjetoHq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoHqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoHqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/joao2dev/ProjetoHq/ProjetoHqApplicationTests.java
@@ -0,0 +1,13 @@
+package joao2dev.ProjetoHq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoHqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
