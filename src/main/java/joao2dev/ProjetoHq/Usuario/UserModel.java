853ac3c7dbe771efa74226e9dc0001aa95a74eae
Joao2dev <beneficil57@gmail.com>
2026-03-09 13:21:26 -0300

    feat: cria a tabela usuarios

---
+++ src/main/java/joao2dev/ProjetoHq/Usuario/UserModel.java
@@ -0,0 +1,29 @@
+package joao2dev.ProjetoHq.Usuario;
+
+import jakarta.persistence.*;
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Table(name = "usuario")
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
---
+++ src/main/resources/db/migration/V5__create_table_usuario.sql
@@ -0,0 +1,6 @@
+CREATE TABLE usuario(
+    id BIGSERIAL NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
