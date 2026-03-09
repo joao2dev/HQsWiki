7640a85a232d835f191e084d359646eb37cf1602
Joao2dev <beneficil57@gmail.com>
2026-03-09 13:19:53 -0300

    fix: adiciona @Builder para melhor fucionamento

--- src/main/java/joao2dev/ProjetoHq/dto/PublisherResponseDTO.java
+++ src/main/java/joao2dev/ProjetoHq/dto/PublisherResponseDTO.java
@@ -1,12 +1,14 @@
 package joao2dev.ProjetoHq.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
+@Builder
 public class PublisherResponseDTO {
     private Long id;
     private String nome;
