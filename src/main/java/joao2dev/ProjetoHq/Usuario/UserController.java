e7c4cdc6bc692ee174024665712671513cb86f41
joao2dev <beneficil57@gmail.com>
2026-04-03 16:18:04 -0300

    feat: adciona contrutores, getter e setters

--- src/main/java/joao2dev/ProjetoHq/Usuario/UserController.java
+++ src/main/java/joao2dev/ProjetoHq/Usuario/UserController.java
@@ -1,10 +1,9 @@
 package joao2dev.ProjetoHq.Usuario;
 
+import joao2dev.ProjetoHq.Services.AuthService;
+import joao2dev.ProjetoHq.dto.LoginRequest;
 import joao2dev.ProjetoHq.dto.UserRequestDTO;
 import joao2dev.ProjetoHq.dto.UserResponseDTO;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +15,21 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     private UserService service;
-
+    private  AuthService authService;
     @PostMapping("/registrar")
     public ResponseEntity<?> registrarUsuario(@RequestBody UserRequestDTO user){
-        service.registrarUsuario(user);
-        return ResponseEntity.ok("usuario registrado com sucesso!");
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.registrarUsuario(user));
+
+
+    }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest user){
+        String token = service.login(user);
+        return ResponseEntity.ok(token);
+    }
+    @GetMapping("/registrar")
+    public String paginaRegistro() {
+        return "auth/registro"; // aponta para templates/auth/registro.html
     }
 }
