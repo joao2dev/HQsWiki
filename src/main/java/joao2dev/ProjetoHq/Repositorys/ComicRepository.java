d232bf9fc5c4f39c78b37511314a82bee8a19cec
joao2dev <beneficil57@gmail.com>
2026-05-25 09:01:10 -0300

    feat: adiciona uma feature de busca parcial ao projeto

--- src/main/java/joao2dev/ProjetoHq/Repositorys/ComicRepository.java
+++ src/main/java/joao2dev/ProjetoHq/Repositorys/ComicRepository.java
@@ -5,9 +5,10 @@ import joao2dev.ProjetoHq.Usuario.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ComicRepository extends JpaRepository<ComicModel , Long> {
 
-   List<ComicModel> findByTituloHqIgnoreCase(String tituloHq);
+   List<ComicModel> findByTituloHqContainingIgnoreCase(String tituloHq);
    List<ComicModel> findByUsuario(UserModel usuario);
 }
